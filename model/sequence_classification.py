94082d887c66b7887a00c514681639b793f3f6c1
Ted Yap <tyy231@nyu.edu>
2022-05-09 19:37:23 -0400

    gitignore

--- model/sequence_classification.py
+++ model/sequence_classification.py
@@ -776,6 +776,7 @@ class RobertaPrefixFusionAttention2ForSequenceClassification(RobertaPreTrainedMo
         self.num_labels = config.num_labels
         self.config = config
         self.roberta = RobertaModel(config)
+        self.embeddings = self.roberta.embeddings
         self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
         self.classifier = torch.nn.Linear(config.hidden_size, config.num_labels)
         self.init_weights()
@@ -788,10 +789,15 @@ class RobertaPrefixFusionAttention2ForSequenceClassification(RobertaPreTrainedMo
         self.n_layer = config.num_hidden_layers
         self.n_head = config.num_attention_heads
         self.n_embd = config.hidden_size // config.num_attention_heads
-
+        # 9: task size
+        kv_dim = config.hidden_size * 10  # 9216
+        self.prompt_attentions = nn.ModuleList(
+            [nn.MultiheadAttention(config.hidden_size, 1, kdim=kv_dim, vdim=kv_dim) for _ in range(self.n_layer)])
         self.prefix_tokens = torch.arange(self.pre_seq_len).long()
         # self.prefix_encoder = PrefixEncoder(config)
-        self.weighted_sum = LinearWeightedSum(2)
+        self.weighted_sum = LinearWeightedSum(9)
+        self.prompt_attentions = nn.ModuleList(
+            [nn.MultiheadAttention(self.n_embd * self.n_head * 9, 1) for _ in range(self.n_layer)])
 
         bert_param = 0
         for name, param in self.roberta.named_parameters():
@@ -807,6 +813,7 @@ class RobertaPrefixFusionAttention2ForSequenceClassification(RobertaPreTrainedMo
 
         self.prompts = get_prompts()
         print('nlayer', self.n_layer)
+        print(f'n_head {self.n_head} n_embd {self.n_embd}')
 
     def get_prompt(self, batch_size):
         prefix_tokens = self.prefix_tokens.unsqueeze(0).expand(batch_size, -1).to(self.roberta.device)
@@ -818,8 +825,6 @@ class RobertaPrefixFusionAttention2ForSequenceClassification(RobertaPreTrainedMo
             self.n_head,
             self.n_embd
         )
-
-        #24 * 2
         past_key_values = self.dropout(past_key_values)
         past_key_values = past_key_values.permute([2, 0, 3, 1, 4]).split(2)
         # split 2 (one for key one for value)
@@ -843,11 +848,56 @@ class RobertaPrefixFusionAttention2ForSequenceClassification(RobertaPreTrainedMo
 
         batch_size = input_ids.shape[0]
         # true_past_key_values = self.get_prompt(batch_size=batch_size)
-        weighted_prompts = self.weighted_sum(self.prompts)
-        weighted_prompts = torch.repeat_interleave(weighted_prompts, batch_size, dim=1)
-        past_key_values = tuple([weighted_prompts for i in range(self.n_layer)])
 
-        prefix_attention_mask = torch.ones(batch_size, self.pre_seq_len).to(self.roberta.device)
+        # rm linear weigh sum; adapt
+        # weighted_prompts = self.weighted_sum(self.prompts)
+        # weighted_prompts = torch.repeat_interleave(weighted_prompts, batch_size, dim=1)
+        # task_size = 12
+        raw_embedding = self.embeddings(
+            input_ids=input_ids,
+            position_ids=position_ids,
+            token_type_ids=token_type_ids,
+        )
+
+        past_key_values = []
+        print(self.prompts.shape, batch_size)
+
+        task_size, n_layer, _, prompt_bz, prompt_n_head, pre_seq_len, n_embed = self.prompts.shape
+
+        for layer in range(self.n_layer):
+            layer_prompt = self.prompts[:, layer, :, :, :, :, :]
+            # dim: [task_size, batch_size, n_head, pre_seq, n_embd]
+
+            kp, vp = layer_prompt[:, 0, :, :, :, :], layer_prompt[:, 1, :, :, :, :]
+            print(kp.shape, vp.shape)
+            kp = kp.permute((3, 1, 4, 2, 0))
+            vp = vp.permute((3, 1, 4, 2, 0))
+
+            # permute: pre_seq, batch, n_embed, n_head, task_size ; squeezed_prompt -> [128, batch_size, embedding_size(64*16)]
+            kp, vp = kp.reshape(pre_seq_len, prompt_bz, -1), vp.reshape(pre_seq_len, prompt_bz, -1)
+            kp = torch.repeat_interleave(kp, batch_size, dim=1)
+            vp = torch.repeat_interleave(vp, batch_size, dim=1)
+            # print(f"ATTENTION: kp shape {kp.shape}, vp shape {vp.shape}, raw embed {raw_embedding.shape}")
+            query_mean_pooled = raw_embedding.permute((1, 0, 2)).mean(dim=0).unsqueeze(0)
+            # print(f" query_mean_pooled{query_mean_pooled.shape}")
+            attn_output, attn_output_weights = self.prompt_attentions[layer](query_mean_pooled, kp, vp)
+            # print(f"attn_output shape {attn_output.shape}")
+            new_k, new_v = attn_output, attn_output.detach().clone()
+
+            new_k = new_k.reshape([batch_size, self.n_head, 1,
+                                   -1])  # [:, :self.n_head, :, :] #pre_seq_len=1, prompt_n_head (now 16) should be n_head = 12
+            new_v = new_v.reshape([batch_size, self.n_head, 1, -1])  # [:, :self.n_head, :, :]
+            # print(f"newk, newv dim {new_k.shape}")
+
+            new_past_kv = torch.stack([new_k, new_v], dim=0)
+            # stack(new_k, new_v) <- [2, batch_size, 16, 128, 64].permute(...) #at the new first dimension
+
+            past_key_values.append(new_past_kv)
+        past_key_values = tuple(past_key_values)
+        print(f'layer {len(past_key_values)}, layer 1 pask kv shape {past_key_values[0].shape}')
+
+        # after mean pooling in Query pre_seq_len =1
+        prefix_attention_mask = torch.ones(batch_size, 1).to(self.roberta.device)
         attention_mask = torch.cat((prefix_attention_mask, attention_mask), dim=1)
 
         outputs = self.roberta(

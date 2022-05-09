9dca48c1bca58cef5f356ab613d87ec8665ee9bd
Ted Yap <tyy231@nyu.edu>
2022-05-09 19:46:18 -0400

    conflict

--- run_fusion_attention1/run_rte_roberta.sh
+++ run_fusion_attention1/run_rte_roberta.sh
@@ -7,7 +7,6 @@ lr=1e-5
 dropout=0.1
 psl=128
 epoch=100
-checkpoints=/scratch/mc8895/checkpoints_1012
 
 python3 train_fusion.py \
   --model_name_or_path roberta-base \
@@ -20,7 +19,7 @@ python3 train_fusion.py \
   --learning_rate $lr \
   --num_train_epochs $epoch \
   --pre_seq_len $psl \
-  --output_dir $checkpoints/$DATASET_NAME-roberta-fusion-attention1/ \
+  --output_dir checkpoints/$DATASET_NAME-roberta-fusion-attention1/ \
   --hidden_dropout_prob $dropout \
   --seed 11 \
   --save_strategy no \

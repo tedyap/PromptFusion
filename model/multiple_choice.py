d3ffddebf3c2d5ba69cb7365733c031ce743fad9
Ted Yap <tyy231@nyu.edu>
2022-05-10 15:30:08 -0400

    change attn2 seq len

--- model/multiple_choice.py
+++ model/multiple_choice.py
@@ -680,17 +680,19 @@ class RobertaPrefixFusionAttention1ForMultipleChoice(RobertaPreTrainedModel):
         )
 
 
-class RobertaPrefixFusionAttention2ForMultipleChoice(RobertaPreTrainedModel):
+class RobertaPrefixFusionAttention2ForSequenceClassification(RobertaPreTrainedModel):
     _keys_to_ignore_on_load_missing = [r"position_ids"]
 
     def __init__(self, config):
         super().__init__(config)
-
+        self.num_labels = config.num_labels
+        self.config = config
         self.roberta = RobertaModel(config)
+        self.embeddings = self.roberta.embeddings
         self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
-        self.classifier = torch.nn.Linear(config.hidden_size, 1)
-
+        self.classifier = torch.nn.Linear(config.hidden_size, config.num_labels)
         self.init_weights()
+        # self.prompt_len = config.prompt_len
 
         for param in self.roberta.parameters():
             param.requires_grad = False
@@ -699,18 +701,31 @@ class RobertaPrefixFusionAttention2ForMultipleChoice(RobertaPreTrainedModel):
         self.n_layer = config.num_hidden_layers
         self.n_head = config.num_attention_heads
         self.n_embd = config.hidden_size // config.num_attention_heads
+        # 9: task size
+        kv_dim = config.hidden_size  # 9216
+        self.prompt_attn_layer1 = nn.ModuleList(
+            [nn.MultiheadAttention(self.n_embd * self.n_head, 1, kdim=kv_dim, vdim=kv_dim) for _ in range(self.n_layer)])
 
-        self.prefix_tokens = torch.arange(self.pre_seq_len).long()
-        self.prefix_encoder = PrefixEncoder(config)
+        self.prompt_attn_layer2 = nn.ModuleList(
+            [nn.MultiheadAttention(self.n_embd * self.n_head, 1, kdim=kv_dim, vdim=kv_dim) for _ in range(self.n_layer)])
 
         bert_param = 0
         for name, param in self.roberta.named_parameters():
             bert_param += param.numel()
         all_param = 0
         for name, param in self.named_parameters():
+            if param.requires_grad:
+                print(name)
+                print(param.numel())
             all_param += param.numel()
         total_param = all_param - bert_param
-        print('total param is {}'.format(total_param))
+        print('total param is {}'.format(total_param))  # 9860105
+
+        self.prompts = get_prompts()
+        self.atten2_seq_len = 32
+
+    def initialize_prompts(self, batch_size, prompt_n_head, pre_seq_len, n_embed):
+        return torch.ones(pre_seq_len, batch_size, prompt_n_head * n_embed, device=self.roberta.device)
 
     def get_prompt(self, batch_size):
         prefix_tokens = self.prefix_tokens.unsqueeze(0).expand(batch_size, -1).to(self.roberta.device)
@@ -724,27 +739,23 @@ class RobertaPrefixFusionAttention2ForMultipleChoice(RobertaPreTrainedModel):
         )
         past_key_values = self.dropout(past_key_values)
         past_key_values = past_key_values.permute([2, 0, 3, 1, 4]).split(2)
+        # split 2 (one for key one for value)
         return past_key_values
 
     def forward(
             self,
             input_ids=None,
-            token_type_ids=None,
+            prompt_list=None,
             attention_mask=None,
-            labels=None,
+            token_type_ids=None,
             position_ids=None,
             head_mask=None,
             inputs_embeds=None,
+            labels=None,
             output_attentions=None,
             output_hidden_states=None,
             return_dict=None,
     ):
-        r"""
-        labels (:obj:`torch.LongTensor` of shape :obj:`(batch_size,)`, `optional`):
-            Labels for computing the multiple choice classification loss. Indices should be in ``[0, ...,
-            num_choices-1]`` where :obj:`num_choices` is the size of the second dimension of the input tensors. (See
-            :obj:`input_ids` above)
-        """
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
         batch_size, num_choices = input_ids.shape[:2] if input_ids is not None else inputs_embeds.shape[:2]
 
@@ -757,41 +768,104 @@ class RobertaPrefixFusionAttention2ForMultipleChoice(RobertaPreTrainedModel):
             if inputs_embeds is not None
             else None
         )
-
-        past_key_values = self.get_prompt(batch_size=batch_size * num_choices)
-        prefix_attention_mask = torch.ones(batch_size * num_choices, self.pre_seq_len).to(self.roberta.device)
-        flat_attention_mask = torch.cat((prefix_attention_mask, flat_attention_mask), dim=1)
-
-        outputs = self.roberta(
-            flat_input_ids,
+        # print(f'batch size {batch_size}, num choice {num_choices}, flat_input_ids{flat_input_ids.shape}, input_ids{input_ids.shape}')
+        # CHANGED everything to flattened
+        raw_embedding = self.embeddings(
+            input_ids=flat_input_ids,
             position_ids=flat_position_ids,
             token_type_ids=flat_token_type_ids,
-            attention_mask=flat_attention_mask,
+        )
+
+        past_key_values = []
+
+        task_size, n_layer, _, prompt_bz, prompt_n_head, pre_seq_len, n_embed = self.prompts.shape
+
+        prompt_init = self.initialize_prompts(batch_size*num_choices, prompt_n_head, self.atten2_seq_len, n_embed)
+
+        for layer in range(self.n_layer):
+            attn_layer1_output, _ = self.prompt_attn_layer1[layer](prompt_init, raw_embedding, raw_embedding)
+
+            # (32, self.atten2_seq_len, 768)
+
+            layer_prompt = self.prompts[:, layer, :, :, :, :, :]
+            # dim: [task_size, batch_size, n_head, pre_seq, n_embd]
+
+            kp, vp = layer_prompt[:, 0, :, :, :, :], layer_prompt[:, 1, :, :, :, :]
+            kp = kp.permute((3, 1, 4, 2, 0))
+            vp = vp.permute((3, 1, 4, 2, 0))
+            # permute: pre_seq, batch, n_embed, n_head, task_size ; squeezed_prompt -> [128, batch_size, embedding_size(64*16)]
+            kp, vp = kp.reshape(pre_seq_len*11, prompt_bz, -1), vp.reshape(pre_seq_len*11, prompt_bz, -1)
+            kp = torch.repeat_interleave(kp, batch_size*num_choices, dim=1)
+            vp = torch.repeat_interleave(vp, batch_size*num_choices, dim=1)
+
+            attn_layer2_output, _ = self.prompt_attn_layer2[layer](attn_layer1_output, kp, vp)
+
+            new_k, new_v = attn_layer2_output, attn_layer2_output.detach().clone()
+
+            new_k = new_k.reshape([self.atten2_seq_len, batch_size, self.n_head, self.n_embd]).permute((1, 2, 0, 3))
+            new_v = new_v.reshape([self.atten2_seq_len, batch_size, self.n_head, self.n_embd]).permute((1, 2, 0, 3))
+
+            # new_k = new_k.reshape([batch_size, self.n_head, 1,
+            #                        -1])  # [:, :self.n_head, :, :] #pre_seq_len=1, prompt_n_head (now 16) should be n_head = 12
+            # new_v = new_v.reshape([batch_size, self.n_head, 1, -1])  # [:, :self.n_head, :, :]
+            # print(f"newk, newv dim {new_k.shape}")
+            # raise
+            new_past_kv = torch.stack([new_k, new_v], dim=0)
+            # stack(new_k, new_v) <- [2, batch_size, 16, 128, 64].permute(...) #at the new first dimension
+
+            past_key_values.append(new_past_kv)
+        past_key_values = tuple(past_key_values)
+
+        prefix_attention_mask = torch.ones(batch_size*num_choices, self.atten2_seq_len).to(self.roberta.device)
+        attention_mask = torch.cat((prefix_attention_mask, attention_mask), dim=1)
+
+        outputs = self.roberta(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
             head_mask=head_mask,
-            inputs_embeds=flat_inputs_embeds,
+            inputs_embeds=inputs_embeds,
             output_attentions=output_attentions,
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
             past_key_values=past_key_values,
         )
+
         pooled_output = outputs[1]
 
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
-        reshaped_logits = logits.view(-1, num_choices)
 
         loss = None
         if labels is not None:
-            loss_fct = CrossEntropyLoss()
-            loss = loss_fct(reshaped_logits, labels)
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
 
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
         if not return_dict:
-            output = (reshaped_logits,) + outputs[2:]
+            output = (logits,) + outputs[2:]
             return ((loss,) + output) if loss is not None else output
 
-        return MultipleChoiceModelOutput(
+        return SequenceClassifierOutput(
             loss=loss,
-            logits=reshaped_logits,
+            logits=logits,
             hidden_states=outputs.hidden_states,
             attentions=outputs.attentions,
         )
--- model/token_classification.py
+++ model/token_classification.py
@@ -629,14 +629,17 @@ class RobertaPrefixFusionAttention1ForTokenClassification(RobertaPreTrainedModel
         )
 
 
-class RobertaPrefixFusionAttention2ForTokenClassification(RobertaPreTrainedModel):
+class RobertaPrefixFusionAttention2ForSequenceClassification(RobertaPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
         self.num_labels = config.num_labels
-        self.roberta = RobertaModel(config, add_pooling_layer=False)
+        self.config = config
+        self.roberta = RobertaModel(config)
+        self.embeddings = self.roberta.embeddings
         self.dropout = torch.nn.Dropout(config.hidden_dropout_prob)
         self.classifier = torch.nn.Linear(config.hidden_size, config.num_labels)
         self.init_weights()
+        # self.prompt_len = config.prompt_len
 
         for param in self.roberta.parameters():
             param.requires_grad = False
@@ -645,18 +648,31 @@ class RobertaPrefixFusionAttention2ForTokenClassification(RobertaPreTrainedModel
         self.n_layer = config.num_hidden_layers
         self.n_head = config.num_attention_heads
         self.n_embd = config.hidden_size // config.num_attention_heads
+        # 9: task size
+        kv_dim = config.hidden_size  # 9216
+        self.prompt_attn_layer1 = nn.ModuleList(
+            [nn.MultiheadAttention(self.n_embd * self.n_head, 1, kdim=kv_dim, vdim=kv_dim) for _ in range(self.n_layer)])
 
-        self.prefix_tokens = torch.arange(self.pre_seq_len).long()
-        self.prefix_encoder = PrefixEncoder(config)
+        self.prompt_attn_layer2 = nn.ModuleList(
+            [nn.MultiheadAttention(self.n_embd * self.n_head, 1, kdim=kv_dim, vdim=kv_dim) for _ in range(self.n_layer)])
 
         bert_param = 0
         for name, param in self.roberta.named_parameters():
             bert_param += param.numel()
         all_param = 0
         for name, param in self.named_parameters():
+            if param.requires_grad:
+                print(name)
+                print(param.numel())
             all_param += param.numel()
         total_param = all_param - bert_param
         print('total param is {}'.format(total_param))  # 9860105
+
+        self.prompts = get_prompts()
+        self.atten2_seq_len = 32
+
+    def initialize_prompts(self, batch_size, prompt_n_head, pre_seq_len, n_embed):
+        return torch.ones(pre_seq_len, batch_size, prompt_n_head * n_embed, device=self.roberta.device)
 
     def get_prompt(self, batch_size):
         prefix_tokens = self.prefix_tokens.unsqueeze(0).expand(batch_size, -1).to(self.roberta.device)
@@ -670,11 +686,13 @@ class RobertaPrefixFusionAttention2ForTokenClassification(RobertaPreTrainedModel
         )
         past_key_values = self.dropout(past_key_values)
         past_key_values = past_key_values.permute([2, 0, 3, 1, 4]).split(2)
+        # split 2 (one for key one for value)
         return past_key_values
 
     def forward(
             self,
             input_ids=None,
+            prompt_list=None,
             attention_mask=None,
             token_type_ids=None,
             position_ids=None,
@@ -688,8 +706,59 @@ class RobertaPrefixFusionAttention2ForTokenClassification(RobertaPreTrainedModel
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         batch_size = input_ids.shape[0]
-        past_key_values = self.get_prompt(batch_size=batch_size)
-        prefix_attention_mask = torch.ones(batch_size, self.pre_seq_len).to(self.roberta.device)
+        # true_past_key_values = self.get_prompt(batch_size=batch_size)
+
+        # rm linear weigh sum; adapt
+        # weighted_prompts = self.weighted_sum(self.prompts)
+        # weighted_prompts = torch.repeat_interleave(weighted_prompts, batch_size, dim=1)
+        # task_size = 12
+        raw_embedding = self.embeddings(
+            input_ids=input_ids,
+            position_ids=position_ids,
+            token_type_ids=token_type_ids,
+        ).permute((1, 0, 2))
+
+        past_key_values = []
+
+        task_size, n_layer, _, prompt_bz, prompt_n_head, pre_seq_len, n_embed = self.prompts.shape
+
+        prompt_init = self.initialize_prompts(batch_size, prompt_n_head, self.atten2_seq_len, n_embed)
+
+        for layer in range(self.n_layer):
+            attn_layer1_output, _ = self.prompt_attn_layer1[layer](prompt_init, raw_embedding, raw_embedding)
+
+            # (32, self.atten2_seq_len, 768)
+
+            layer_prompt = self.prompts[:, layer, :, :, :, :, :]
+            # dim: [task_size, batch_size, n_head, pre_seq, n_embd]
+
+            kp, vp = layer_prompt[:, 0, :, :, :, :], layer_prompt[:, 1, :, :, :, :]
+            kp = kp.permute((3, 1, 4, 2, 0))
+            vp = vp.permute((3, 1, 4, 2, 0))
+            # permute: pre_seq, batch, n_embed, n_head, task_size ; squeezed_prompt -> [128, batch_size, embedding_size(64*16)]
+            kp, vp = kp.reshape(pre_seq_len*11, prompt_bz, -1), vp.reshape(pre_seq_len*11, prompt_bz, -1)
+            kp = torch.repeat_interleave(kp, batch_size, dim=1)
+            vp = torch.repeat_interleave(vp, batch_size, dim=1)
+
+            attn_layer2_output, _ = self.prompt_attn_layer2[layer](attn_layer1_output, kp, vp)
+
+            new_k, new_v = attn_layer2_output, attn_layer2_output.detach().clone()
+
+            new_k = new_k.reshape([self.atten2_seq_len, batch_size, self.n_head, self.n_embd]).permute((1, 2, 0, 3))
+            new_v = new_v.reshape([self.atten2_seq_len, batch_size, self.n_head, self.n_embd]).permute((1, 2, 0, 3))
+
+            # new_k = new_k.reshape([batch_size, self.n_head, 1,
+            #                        -1])  # [:, :self.n_head, :, :] #pre_seq_len=1, prompt_n_head (now 16) should be n_head = 12
+            # new_v = new_v.reshape([batch_size, self.n_head, 1, -1])  # [:, :self.n_head, :, :]
+            # print(f"newk, newv dim {new_k.shape}")
+            # raise
+            new_past_kv = torch.stack([new_k, new_v], dim=0)
+            # stack(new_k, new_v) <- [2, batch_size, 16, 128, 64].permute(...) #at the new first dimension
+
+            past_key_values.append(new_past_kv)
+        past_key_values = tuple(past_key_values)
+
+        prefix_attention_mask = torch.ones(batch_size, self.atten2_seq_len).to(self.roberta.device)
         attention_mask = torch.cat((prefix_attention_mask, attention_mask), dim=1)
 
         outputs = self.roberta(
@@ -705,30 +774,38 @@ class RobertaPrefixFusionAttention2ForTokenClassification(RobertaPreTrainedModel
             past_key_values=past_key_values,
         )
 
-        sequence_output = outputs[0]
-        sequence_output = self.dropout(sequence_output)
-        logits = self.classifier(sequence_output)
-        attention_mask = attention_mask[:, self.pre_seq_len:].contiguous()
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
 
         loss = None
         if labels is not None:
-            loss_fct = CrossEntropyLoss()
-            # Only keep active parts of the loss
-            if attention_mask is not None:
-                active_loss = attention_mask.view(-1) == 1
-                active_logits = logits.view(-1, self.num_labels)
-                active_labels = torch.where(
-                    active_loss, labels.view(-1), torch.tensor(loss_fct.ignore_index).type_as(labels)
-                )
-                loss = loss_fct(active_logits, active_labels)
-            else:
-                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
 
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
         if not return_dict:
             output = (logits,) + outputs[2:]
             return ((loss,) + output) if loss is not None else output
 
-        return TokenClassifierOutput(
+        return SequenceClassifierOutput(
             loss=loss,
             logits=logits,
             hidden_states=outputs.hidden_states,
